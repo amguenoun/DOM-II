565db6a652886352cfee14f238bd975a51a31b8b
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-06 15:03:40 -0500

    added first eventListener, a mouseenter event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+const navBar = document.querySelectorAll('.nav-link');
+navBar.forEach(item => {
+    item.addEventListener('mouseenter', (event) => {
+        event.target.style.background = "black";
+        event.target.style.color = "white";
+        event.target.style.padding = "1% 2%";
+    });
+});
