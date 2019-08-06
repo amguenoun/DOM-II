dba3f08fd5c5c5f078b664cd6882822949caa8c7
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-06 18:44:14 -0500

    couldnt finish last project objective

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -5,4 +5,11 @@ blocks.forEach((item) => {
         event.target.remove();
         blocksContainer.prepend(event.target);
     });
+    item.addEventListener('mousedown', (event) => {
+        TweenMax.to(event.target, 1, {
+            x: 1000,
+            ease: Elastic.easeOut.config(1, 0.75)
+        })
+
+    });
 });
